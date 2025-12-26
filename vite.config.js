74f4c6c7339e48418198390ae8547417d6cfc0aa
Prajwal kumar <uppalaprajwalkumar@gmail.com>
2025-12-26 18:51:55 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures that assets are loaded relative to the current path
+  build: {
+    assetsDir: 'assets', // This is where your built assets will go
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
 
